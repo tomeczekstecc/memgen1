10dcd7384640eeb0704edb54d8ceb2e14d13c681
Steć Tomasz <stect@slaskie.pl>
2019-09-06 11:43:32 +0200

    first

---
+++ build/precache-manifest.18fb5d91fe4397fe344902e54d7eb086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9827319caf3ed15d9e308a8944bed44a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94cf0a81a69996ba036a",
+    "url": "/static/css/main.154ea31e.chunk.css"
+  },
+  {
+    "revision": "5fae7632446a417c0ecf",
+    "url": "/static/js/2.99e7cb5b.chunk.js"
+  },
+  {
+    "revision": "94cf0a81a69996ba036a",
+    "url": "/static/js/main.59379843.chunk.js"
+  },
+  {
+    "revision": "3ef4d0dfc0543e05b64a",
+    "url": "/static/js/runtime~main.f118fd86.js"
+  }
+]);
